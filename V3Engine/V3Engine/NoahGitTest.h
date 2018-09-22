d526a65661f2b3a02d99a853c4a213c3c3694c20
Noah Bandl <grumpyonion82@hotmail.com>
2018-09-22 16:38:15 -0400

    - added comments for conflict testing

--- V3Engine/V3Engine/NoahGitTest.h
+++ V3Engine/V3Engine/NoahGitTest.h
@@ -1,12 +1,12 @@
 #ifndef NOAHGITTEST_H
 #define NOAHGITTEST_H
-
+//Noah comment 1
 struct NoahGitTest {
 
 	static inline void test() {
 		//literally nothing here go away
 	}
-
+	//Noah comment 2
 };
 
 #endif // ! NOAHGITTEST_H
